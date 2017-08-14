45471d9d4d3c2c3591e576dc83e9ea7a405332f3
Collin OConnell <ococncol@gmail.com>
2017-08-14 09:59:16 -0400

    init commit w/ few files extra

---
+++ app/src/main/java/design/shortnd/basicicelandic/MainActivity.java
@@ -0,0 +1,19 @@
+package design.shortnd.basicicelandic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: 1. Get first fragment hosted in main activity
+
+    //TODO: 2. Implement a Recycler View to Display our list
+
+    //TODO: 3. Start with Phrases first
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/design/shortnd/basicicelandic/PhrasesFragment.java
@@ -0,0 +1,11 @@
+package design.shortnd.basicicelandic;
+
+import android.support.v4.app.Fragment;
+
+
+public class PhrasesFragment extends Fragment {
+    // Needs to be here
+    public PhrasesFragment() {}
+
+
+}
